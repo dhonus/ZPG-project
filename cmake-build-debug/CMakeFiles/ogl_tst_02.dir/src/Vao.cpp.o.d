942bd68b1349588e1221c626799104111a3fddba
Daniel Honus <dhonus@tuta.io>
2022-09-30 23:10:51 +0200

    mhm

---
+++ cmake-build-debug/CMakeFiles/ogl_tst_02.dir/src/Vao.cpp.o.d
@@ -0,0 +1,78 @@
+CMakeFiles/ogl_tst_02.dir/src/Vao.cpp.o: \
+ /home/daniel/Documents/workspace/school/ZPG/ZPG-project/src/Vao.cpp \
+ /usr/include/stdc-predef.h \
+ /home/daniel/Documents/workspace/school/ZPG/ZPG-project/src/../include/Vao.h \
+ /usr/include/GL/glew.h /usr/include/c++/12.2.0/cstddef \
+ /usr/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/12.2.0/pstl/pstl_config.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/stddef.h \
+ /usr/include/c++/12.2.0/cstdint \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/12.2.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/GL/glu.h /usr/include/GL/gl.h \
+ /home/daniel/Documents/workspace/school/ZPG/ZPG-project/src/../include/Vbo.h \
+ /usr/include/GLFW/glfw3.h /usr/include/c++/12.2.0/vector \
+ /usr/include/c++/12.2.0/bits/stl_algobase.h \
+ /usr/include/c++/12.2.0/bits/functexcept.h \
+ /usr/include/c++/12.2.0/bits/exception_defines.h \
+ /usr/include/c++/12.2.0/bits/cpp_type_traits.h \
+ /usr/include/c++/12.2.0/ext/type_traits.h \
+ /usr/include/c++/12.2.0/ext/numeric_traits.h \
+ /usr/include/c++/12.2.0/bits/stl_pair.h \
+ /usr/include/c++/12.2.0/type_traits /usr/include/c++/12.2.0/bits/move.h \
+ /usr/include/c++/12.2.0/bits/utility.h \
+ /usr/include/c++/12.2.0/bits/stl_iterator_base_types.h \
+ /usr/include/c++/12.2.0/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/12.2.0/bits/concept_check.h \
+ /usr/include/c++/12.2.0/debug/assertions.h \
+ /usr/include/c++/12.2.0/bits/stl_iterator.h \
+ /usr/include/c++/12.2.0/bits/ptr_traits.h \
+ /usr/include/c++/12.2.0/debug/debug.h \
+ /usr/include/c++/12.2.0/bits/predefined_ops.h \
+ /usr/include/c++/12.2.0/bits/allocator.h \
+ /usr/include/c++/12.2.0/x86_64-pc-linux-gnu/bits/c++allocator.h \
+ /usr/include/c++/12.2.0/bits/new_allocator.h /usr/include/c++/12.2.0/new \
+ /usr/include/c++/12.2.0/bits/exception.h \
+ /usr/include/c++/12.2.0/bits/memoryfwd.h \
+ /usr/include/c++/12.2.0/bits/stl_construct.h \
+ /usr/include/c++/12.2.0/bits/stl_uninitialized.h \
+ /usr/include/c++/12.2.0/ext/alloc_traits.h \
+ /usr/include/c++/12.2.0/bits/alloc_traits.h \
+ /usr/include/c++/12.2.0/bits/stl_vector.h \
+ /usr/include/c++/12.2.0/initializer_list \
+ /usr/include/c++/12.2.0/bits/stl_bvector.h \
+ /usr/include/c++/12.2.0/bits/functional_hash.h \
+ /usr/include/c++/12.2.0/bits/hash_bytes.h \
+ /usr/include/c++/12.2.0/bits/refwrap.h \
+ /usr/include/c++/12.2.0/bits/invoke.h \
+ /usr/include/c++/12.2.0/bits/stl_function.h \
+ /usr/include/c++/12.2.0/backward/binders.h \
+ /usr/include/c++/12.2.0/bits/range_access.h \
+ /usr/include/c++/12.2.0/bits/vector.tcc /usr/include/glm/vec4.hpp \
+ /usr/include/glm/ext/vector_bool4.hpp \
+ /usr/include/glm/detail/type_vec4.hpp \
+ /usr/include/glm/detail/qualifier.hpp /usr/include/glm/detail/setup.hpp \
+ /usr/include/c++/12.2.0/cassert /usr/include/assert.h \
+ /usr/include/glm/simd/platform.h /usr/include/glm/detail/type_vec4.inl \
+ /usr/include/glm/detail/compute_vector_relational.hpp \
+ /usr/include/c++/12.2.0/limits \
+ /usr/include/glm/ext/vector_bool4_precision.hpp \
+ /usr/include/glm/ext/vector_float4.hpp \
+ /usr/include/glm/ext/vector_float4_precision.hpp \
+ /usr/include/glm/ext/vector_double4.hpp \
+ /usr/include/glm/ext/vector_double4_precision.hpp \
+ /usr/include/glm/detail/setup.hpp /usr/include/glm/ext/vector_int4.hpp \
+ /usr/include/glm/ext/vector_int4_sized.hpp \
+ /usr/include/glm/ext/scalar_int_sized.hpp \
+ /usr/include/glm/ext/vector_uint4.hpp \
+ /usr/include/glm/ext/vector_uint4_sized.hpp \
+ /usr/include/glm/ext/scalar_uint_sized.hpp
